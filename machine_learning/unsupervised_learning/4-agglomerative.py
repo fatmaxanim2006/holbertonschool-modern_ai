a2d15a75687c53b00497c0b99919307f83d522e0
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-02 21:44:54 +0000

    Fix Agglomerative_Clustering logic and return values

--- machine_learning/unsupervised_learning/4-agglomerative.py
+++ machine_learning/unsupervised_learning/4-agglomerative.py
@@ -4,25 +4,28 @@ from sklearn import cluster
 from sklearn import metrics
 
 
-def Agglomerative_Clustering(X, n_clusters, random_state, n_components, use_pca_data=True):
+def Agglomerative_Clustering(X, n_clusters, random_state=0, n_components=0):
     """
     Performs Agglomerative hierarchical clustering.
     """
+    # Dinamik import tələbi
     Apply_PCA = __import__('1-pca').Apply_PCA
     
-    # 1. Dimensiya azaldılması (PCA)
-    if use_pca_data:
+    # 1. Dimensiya azaldılması
+    if n_components > 0:
         X_used, _ = Apply_PCA(X, n_components=n_components, random_state=random_state)
     else:
         X_used = X
         
-    # 2. Klasterləşdirmə (Ward linkage)
+    # 2. Klasterləşdirmə (ward linkage standartdır)
     model = cluster.AgglomerativeClustering(n_clusters=n_clusters, linkage='ward')
     model.fit(X_used)
     
-    # 3. Qiymətləndirmə (Silhouette score)
-    score = None
+    # 3. Silhouette score hesablama
+    # n_clusters=1 olduqda silhouette_score işləmir, ona görə None qaytarırıq
     if n_clusters > 1:
         score = metrics.silhouette_score(X_used, model.labels_)
+    else:
+        score = None
         
     return model, X_used, score
