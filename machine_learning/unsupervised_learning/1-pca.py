830c9e3672f5ec2475592464598c6eee2daec873
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-02 20:54:16 +0000

    Add 1-pca.py solution

--- machine_learning/unsupervised_learning/1-pca.py
+++ machine_learning/unsupervised_learning/1-pca.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""Module for PCA application"""
+"""Module for PCA dimensionality reduction"""
 from sklearn import decomposition
 
-def Apply_PCA(X, n_components, random_state):
+
+def Apply_PCA(X, n_components=None, random_state=None):
     """Performs PCA on tabular data using Scikit-learn"""
-    pca = decomposition.PCA(n_components=n_components, random_state=random_state)
-    transformed_data = pca.fit_transform(X)
-    return transformed_data, pca
+    pca = decomposition.PCA(n_components=n_components,
+                            random_state=random_state)
+    return pca.fit_transform(X), pca
