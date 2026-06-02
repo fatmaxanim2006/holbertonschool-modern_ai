0a9c3b0b7403b14f03bb890b9a16b37eb64400dc
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-02 21:22:33 +0000

    Fix import and logic for optimal_k

--- machine_learning/unsupervised_learning/3-optimal_k.py
+++ machine_learning/unsupervised_learning/3-optimal_k.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 """Module for finding optimal K"""
-from sklearn.cluster import KMeans
 from sklearn import metrics
 
-
-def optimal_k(X, max_clusters=None, random_state=0):
+def optimal_k(X, max_clusters, random_state):
     """Evaluates K-Means clustering quality"""
+    K_Means = __import__('2-k_means').K_Means
+    
     ks = []
     inertia_values = []
     silhouette_scores = []
-
+    
     for k in range(2, max_clusters + 1):
-        model = KMeans(n_clusters=k, random_state=random_state)
+        model = K_Means(n_clusters=k, random_state=random_state)
         model.fit(X)
         ks.append(k)
         inertia_values.append(model.inertia_)
         silhouette_scores.append(metrics.silhouette_score(X, model.labels_))
+        
     return ks, inertia_values, silhouette_scores
