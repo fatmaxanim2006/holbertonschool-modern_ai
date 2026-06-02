d13f67df0134a5e0654f831074011c890b79797f
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-02 21:24:15 +0000

    Fix import and style issues

--- machine_learning/unsupervised_learning/3-optimal_k.py
+++ machine_learning/unsupervised_learning/3-optimal_k.py
@@ -2,19 +2,18 @@
 """Module for finding optimal K"""
 from sklearn import metrics
 
-def optimal_k(X, max_clusters, random_state):
+
+def optimal_k(X, max_clusters=None, random_state=0):
     """Evaluates K-Means clustering quality"""
     K_Means = __import__('2-k_means').K_Means
-    
     ks = []
     inertia_values = []
     silhouette_scores = []
-    
+
     for k in range(2, max_clusters + 1):
         model = K_Means(n_clusters=k, random_state=random_state)
         model.fit(X)
         ks.append(k)
         inertia_values.append(model.inertia_)
         silhouette_scores.append(metrics.silhouette_score(X, model.labels_))
-        
     return ks, inertia_values, silhouette_scores
