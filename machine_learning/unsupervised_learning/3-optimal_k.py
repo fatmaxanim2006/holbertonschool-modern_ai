462225dcad0b0a3ee423849df5145576da45f236
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-02 21:11:50 +0000

    Fix optimal_k style and logic

--- machine_learning/unsupervised_learning/3-optimal_k.py
+++ machine_learning/unsupervised_learning/3-optimal_k.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for finding optimal K"""
+from sklearn.cluster import KMeans
 from sklearn import metrics
-K_Means = __import__('2-kmeans').kmeans
 
 
 def optimal_k(X, max_clusters=None, random_state=0):
@@ -11,17 +11,9 @@ def optimal_k(X, max_clusters=None, random_state=0):
     silhouette_scores = []
 
     for k in range(2, max_clusters + 1):
-        # K-Means modelini əvvəlki tapşırıqdakı funksiya ilə yaradırıq
-        labels, centers = K_Means(X, k)
-
-        # Inertia hesablanması
-        # Qeyd: sklearn.cluster.KMeans obyektini tələb edir
-        from sklearn.cluster import KMeans
         model = KMeans(n_clusters=k, random_state=random_state)
         model.fit(X)
-        
         ks.append(k)
         inertia_values.append(model.inertia_)
         silhouette_scores.append(metrics.silhouette_score(X, model.labels_))
-
     return ks, inertia_values, silhouette_scores
