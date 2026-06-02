1c4a2b32f81373ab71be9da51d03fb93fbfa47b8
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-02 20:50:47 +0000

    Fix pycodestyle E302 error

--- machine_learning/unsupervised_learning/0-standardize.py
+++ machine_learning/unsupervised_learning/0-standardize.py
@@ -2,6 +2,7 @@
 """Module for data standardization"""
 from sklearn import preprocessing
 
+
 def Standardize(x):
     """Standardizes input data using StandardScaler"""
     scaler = preprocessing.StandardScaler()
