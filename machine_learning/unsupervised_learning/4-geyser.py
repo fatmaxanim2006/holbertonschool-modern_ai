13485be453ca4eac254f5a1f7668ded804572be5
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-03 16:15:07 +0000

    Fix: Update 4-geyser.py with correct attribute names

--- machine_learning/unsupervised_learning/4-geyser.py
+++ machine_learning/unsupervised_learning/4-geyser.py
@@ -6,7 +6,9 @@ K_Means = __import__('2-k_means').K_Means
 def geyser():
     """Performs K-Means clustering on the geyser dataset"""
     data = np.load('geyser.npy')
-    # n_clusters=3 olduğu tələb olunur
+    
+    # 3 klaster üçün modeli işə salırıq
     model = K_Means(data, n_clusters=3)
-    # labels_ və centroids_ tələb olunur
+    
+    # Düzgün atribut adları (scikit-learn standartı olan '_' ilə bitənlər)
     return model.labels_, model.cluster_centers_
