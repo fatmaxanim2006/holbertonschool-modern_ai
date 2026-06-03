f1021b4f625e742cd26c535b03769976a2abb3cf
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-03 16:12:23 +0000

    Fix: Final implementation of 4-geyser.py

--- machine_learning/unsupervised_learning/4-geyser.py
+++ machine_learning/unsupervised_learning/4-geyser.py
@@ -5,13 +5,8 @@ K_Means = __import__('2-k_means').K_Means
 
 def geyser():
     """Performs K-Means clustering on the geyser dataset"""
-    # Verilənləri yükləyirik
     data = np.load('geyser.npy')
-    
-    # K-Means modelini qururuq (tələb olunan k=3 klaster üçün)
+    # n_clusters=3 olduğu tələb olunur
     model = K_Means(data, n_clusters=3)
-    
-    # Klasterləşdirməni yerinə yetiririk
-    labels = model.labels_
-    
-    return labels, model
+    # labels_ və centroids_ tələb olunur
+    return model.labels_, model.cluster_centers_
