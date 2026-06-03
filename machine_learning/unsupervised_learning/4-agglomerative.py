a0b6509759d0fbe20d08961ec48f93d369dc19d9
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-03 16:48:39 +0000

    Add Agglomerative Hierarchical Clustering function

--- machine_learning/unsupervised_learning/4-agglomerative.py
+++ machine_learning/unsupervised_learning/4-agglomerative.py
@@ -24,14 +24,11 @@ def Agglomerative_Clustering(X, n_clusters, random_state,
         float: Silhouette score (None if n_clusters == 1)
     """
     if use_pca_data:
-        X_used, _ = Apply_PCA(X, n_components=n_components,
-                               random_state=random_state)
+        X_used, _ = Apply_PCA(X, n_components=n_components, random_state=random_state)
     else:
         X_used = X
 
-    model = cluster.AgglomerativeClustering(
-        n_clusters=n_clusters, linkage='ward'
-    )
+    model = cluster.AgglomerativeClustering(n_clusters=n_clusters, linkage='ward')
     model.fit(X_used)
 
     if n_clusters > 1:
