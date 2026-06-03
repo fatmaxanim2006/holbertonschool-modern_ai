1174592b946d093c221735274fa4fcb6f44110b7
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-03 16:26:58 +0000

    Fix: Final version of Agglomerative_Clustering

--- machine_learning/unsupervised_learning/4-agglomerative.py
+++ machine_learning/unsupervised_learning/4-agglomerative.py
@@ -4,27 +4,20 @@ from sklearn import cluster
 from sklearn import metrics
 
 
-def Agglomerative_Clustering(X, n_clusters, random_state=0, n_components=0):
-    """
-    Performs Agglomerative hierarchical clustering.
-    """
-    # Dinamik import
+def Agglomerative_Clustering(X, n_clusters, random_state, n_components, use_pca_data=True):
+    """Performs Agglomerative hierarchical clustering."""
     Apply_PCA = __import__('1-pca').Apply_PCA
-    
-    # PCA tətbiqi yalnız n_components > 0 olduqda baş verməlidir
-    if n_components > 0:
+
+    if use_pca_data:
         X_used, _ = Apply_PCA(X, n_components=n_components, random_state=random_state)
     else:
-        # PCA tətbiq olunmadıqda orijinal X istifadə olunur
         X_used = X
-        
-    # Klasterləşdirmə
+
     model = cluster.AgglomerativeClustering(n_clusters=n_clusters, linkage='ward')
     model.fit(X_used)
-    
-    # Silhouette score hesablama (n_clusters > 1 şərti ilə)
+
     score = None
     if n_clusters > 1:
         score = metrics.silhouette_score(X_used, model.labels_)
-        
+
     return model, X_used, score
